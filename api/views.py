2b6cb17f4d7a15dc486f69b2faa36f6b4717bd54
zahur <zahurmeerun@hotmail.com>
2022-03-31 17:40:27 +0400

    update: create serializer

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse
 
+from .models import Character
+
 
 # Create your views here
 def all_characters(request):
-    return JsonResponse({'name': 'zahur'})
+    all_characters = list(Character.objects.all().values('name', 'gender', 'birth_year', 'birth_planet', 'faction' ))
+    print(all_characters)
+    return JsonResponse({'request': all_characters})
