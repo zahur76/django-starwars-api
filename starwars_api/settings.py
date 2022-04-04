9f713d6e788e322567c7bc1cea402cb186ce25b9
zahur <zahurmeerun@hotmail.com>
2022-04-04 16:14:04 +0400

    update: settings for allowed hosts

--- starwars_api/settings.py
+++ starwars_api/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-starwars-api.herokuapp.com", "127.0.0.1", 'http://localhost:8000/']
 
 
 # Application definition
