6dacb6180cb58b043077b6cfa956b50c323691ad
zahur <zahurmeerun@hotmail.com>
2022-04-04 16:11:37 +0400

    update: settings for collectstatic

--- starwars_api/settings.py
+++ starwars_api/settings.py
@@ -134,6 +134,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# used for testing collectstatic in development
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
