1c5c8538721a5bcc2738f65c6bb1083e72db2011
zahur <zahurmeerun@hotmail.com>
2022-08-08 18:35:38 +0400

    update: media settinsg to allow local media storage

--- starwars_api/settings.py
+++ starwars_api/settings.py
@@ -174,6 +174,9 @@ STATIC_URL = 'static/'
 # used for testing collectstatic in development
 STATIC_ROOT = '/home/hanssqrd/starwarsAPI.hansolo.digital/static'
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- starwars_api/urls.py
+++ starwars_api/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
